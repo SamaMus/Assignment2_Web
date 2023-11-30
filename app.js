1c1b56968712f98326861c6b904ab79148ba3cb8
Sama <mustafalinihad8@gmail.com>
2023-11-30 21:01:16 +0400

    display

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Fetch data from the API
     fetch('https://dummyjson.com/products')
         .then((response) => response.json())
         .then((data) => {
-            // Store the product data globally for search and filter functionality
             window.productData = data.products;
 
-            // Display products on the home page
-            displayProducts(window.productData, 1); // Display the first page initially
+            displayProducts(window.productData, 1);
 
-            // Implement search, filter, and pagination functionality
-            // You can add event listeners and functions here
             document.getElementById('searchInput').addEventListener('input', handleSearch);
             document.getElementById('categoryFilter').addEventListener('change', handleFilterChange);
             document.getElementById('minPrice').addEventListener('input', handleFilterChange);
             document.getElementById('maxPrice').addEventListener('input', handleFilterChange);
 
-            // Populate category filter options
             populateCategoryFilter(data.categories);
 
-            // Display the selected filters
             displaySelectedFilters();
         })
         .catch((error) => console.error('Error fetching data:', error));
 });
+
+function displayProducts(products, currentPage) {
+    const productList = document.getElementById('productList');
+    productList.innerHTML = '';
+
+    const itemsPerPage = 10;
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    for (let i = startIndex; i < endIndex && i < products.length; i++) {
+        const productCard = createProductCard(products[i]);
+        productList.appendChild(productCard);
+    }
+
+    displayPagination(products.length, itemsPerPage, currentPage);
+}
