0d6b239e10c8b537d7b604d06ac04095b2e1ef4d
Sama <mustafalinihad8@gmail.com>
2023-12-02 15:02:05 +0400

    rendering details

--- Details_Page/details.js
+++ Details_Page/details.js
@@ -14,3 +14,45 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => console.error('Error fetching product details:', error));
 });
+
+const renderProductDetails = (product) => {
+    const productTitle = document.getElementById('product-title');
+    productTitle.textContent = `${product.title} Details`;
+
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = '';
+
+    const detailsCard = document.createElement('div');
+    detailsCard.className = 'details-card';
+
+    const detailsImage = document.createElement('img');
+    detailsImage.src = product.thumbnail;
+    detailsImage.alt = product.title;
+
+    const detailsContent = document.createElement('div');
+    detailsContent.className = 'details-content';
+
+    const title = document.createElement('h2');
+    title.textContent = product.title;
+
+    const description = document.createElement('h3');
+    description.textContent = product.description;
+
+    const price = document.createElement('p');
+    price.className = 'price';
+    price.textContent = `Price: ${product.price} Afg`;
+
+    const discount = document.createElement('p');
+    discount.className = 'discount';
+    discount.textContent = `Discount: ${product.discountPercentage}%`;
+
+    const stock = document.createElement('p');
+    stock.className = 'stock';
+    stock.textContent = `In Stock: ${product.stock} units`;
+
+    const rating = document.createElement('p');
+    rating.className = 'rating';
+    rating.textContent = `Rating: ${product.rating}/5`;
+
+    
+};
